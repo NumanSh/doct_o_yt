201704d1eeb66da8d2670d45e35ad83fc65b9c4c
DESKTOP-MR49FIL\Asus <numan.sh.0101@gmail.com>
2023-12-30 00:53:38 +0200

    splash screen for android 12 and above

--- lib/main.dart
+++ lib/main.dart
@@ -8,3 +8,10 @@ void main() {
     appRoutes: AppRoutes(),
   ));
 }
+// here is an import note:
+// i used the flutter_native_splash package to add a splash screen to the app, first i created a new file called dlutter_native_splash.yaml
+// i had a problem about the android that had Api 30 or above so there is a way to solve it, using figma.
+// first do a frame with width and heigth 640 and radius 320 then make them  group.
+// then make another frame with 960 and put the group inside the it. 
+//import it as png with 1x. and use it.
+//dont forgrt to use dart run flutter_native_splash:create in the terminal. 
